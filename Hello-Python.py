c9454e0c01bc141d0857b8a578034c104d4f4394
Ani <aprodanova3@gmail.com>
2025-05-09 16:00:40 +0100

    Panos instructions

---
+++ Hello-Python.py
@@ -0,0 +1,4 @@
+
+sql, dbt, ftp server will be airflow (python) run the transformations,
+GCS bucket stored , data warehouse from gcs bucket,
+Docker -(application containers) has airflow installed; then write pipeline; deployment !!!! Also BigQuery
