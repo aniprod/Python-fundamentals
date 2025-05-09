b995a268d771c8d21ce0572f1c30bb9845236794
Ani <aprodanova3@gmail.com>
2025-05-09 16:01:54 +0100

    courses from Panos

--- Hello-Python.py
+++ Hello-Python.py
@@ -2,3 +2,12 @@
 sql, dbt, ftp server will be airflow (python) run the transformations,
 GCS bucket stored , data warehouse from gcs bucket,
 Docker -(application containers) has airflow installed; then write pipeline; deployment !!!! Also BigQuery
+
+Resources Panos told me to use:
+https://dev.to/markbdsouza/apache-airflow-for-beginners-16o
+ 
+https://theaisummer.com/apache-airflow-tutorial/
+ 
+https://blog.adnansiddiqi.me/using-apache-airflow-etl-to-fetch-and-analyze-btc-data/
+ 
+https://www.franciscoyira.com/post/data-pipelines-cloud-intro-airflow-docker/#installing-airflow-on-the-windows-subsystem-for-linux
