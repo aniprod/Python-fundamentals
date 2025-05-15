5a13d51a180fca785839ba0b0233fc381dcbe305
Ani <aprodanova3@gmail.com>
2025-05-15 17:39:42 +0100

    uploaded cutomers_TEST1 file to GCS after many tries to create a BigQuery Dataset. Success finally!

--- upload-to-cloud.py
+++ upload-to-cloud.py
@@ -10,8 +10,8 @@ def upload_to_gcs(bucket_name, source_file_path, destination_blob_name):
 
 def main():
     bucket_name = "ani_test_bucket"  
-    source_file_path = "/Users/aniprodanova/Desktop/Python-fundamentals/customers.csv"  
-    destination_blob_name = "raw/customers.csv"  
+    source_file_path = "/Users/aniprodanova/Desktop/Python-fundamentals/Customers_TEST1.csv"  
+    destination_blob_name = "raw/Customers_TEST1.csv"  
 
     if not os.path.exists(source_file_path):
         print(f"Error: File not found at {source_file_path}")
@@ -20,7 +20,7 @@ def main():
     upload_to_gcs(bucket_name, source_file_path, destination_blob_name)
 
 if __name__ == "__main__":
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/aniprodanova/Desktop/Python-fundamentals/de-1stProject-key.json"  
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/aniprodanova/Desktop/Python-fundamentals/de-1st-project-key.json"  
     main()
 
 
