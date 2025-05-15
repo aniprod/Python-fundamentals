f4ca3f54c6ef4696fa72ce54b5a4af819b68b9a9
Ani <aprodanova3@gmail.com>
2025-05-15 12:09:52 +0100

    uploaded a dataset to gsc bucket

---
+++ upload-to-cloud.py
@@ -0,0 +1,29 @@
+import os
+from google.cloud import storage
+
+def upload_to_gcs(bucket_name, source_file_path, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_path)
+    print(f"File uploaded to gs://{bucket_name}/{destination_blob_name}.")
+
+def main():
+    bucket_name = "ani_test_bucket"  
+    source_file_path = "/Users/aniprodanova/Desktop/Python-fundamentals/customers.csv"  
+    destination_blob_name = "raw/customers.csv"  
+
+    if not os.path.exists(source_file_path):
+        print(f"Error: File not found at {source_file_path}")
+        return
+
+    upload_to_gcs(bucket_name, source_file_path, destination_blob_name)
+
+if __name__ == "__main__":
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/aniprodanova/Desktop/Python-fundamentals/de-1stProject-key.json"  
+    main()
+
+
+
+
+       
